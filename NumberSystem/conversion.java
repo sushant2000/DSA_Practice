b355309b1a482dfe0495c079b9a52e2d02fcb62f
sushant2000 <sushantmawar10@gmail.com>
2021-07-31 22:24:47 +0530

    update

--- NumberSystem/conversion.java
+++ NumberSystem/conversion.java
@@ -6,7 +6,7 @@ public class conversion {
  private static int digit_frequency(int num , int digit){
       int count = 0;
       while(num>0){
-          int d = num%10;
+          long d = num%10;
           num = num/10;
           if(d==digit){
               count++;
@@ -15,10 +15,10 @@ public class conversion {
 return count;
  }
 
- private static long decimalToAnyBase(int num , int base){
+ private static long decimalToAnyBase(long num , int base){
      long ans = 0, p = 1;
      while(num>0){
-         int rem = num % base;
+         long rem = num % base;
          num = num / base;
 
          ans = rem * p + ans;
@@ -28,10 +28,10 @@ return count;
  }
 
 
-private static long anyBaseTodecimal(int num , int base){
+private static long anyBaseTodecimal(long num , int base){
     long ans = 0 , p = 1;
     while(num>0){
-        int dig = num % 10;
+        long dig = num % 10;
         num = num/10;
 
         ans = ans + dig*p;
@@ -40,13 +40,23 @@ private static long anyBaseTodecimal(int num , int base){
     return ans;
 }
 
+private static long anyBaseToAnyBase(int num , int given_base , int req_base){
+    
+   long temp = anyBaseTodecimal(num , given_base);
+   long ans = decimalToAnyBase(temp , req_base); 
+    return ans;
+}
 
-    public static void main(String[] args){
+
+
+public static void main(String[] args){
   int n = scn.nextInt();
-  int b = scn.nextInt();
+  int b1 = scn.nextInt();
+  int b2 = scn.nextInt();
  // int freq = digit_frequency(n, d);
   //long freq = decimalToAnyBase(n, b);
-   long freq = anyBaseTodecimal(n, b);
+  // long freq = anyBaseTodecimal(n, b);
+   long freq = anyBaseToAnyBase(n, b1 , b2);
   System.out.println(freq); 
 
     }
